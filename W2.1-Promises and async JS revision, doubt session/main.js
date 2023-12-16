1bc6a6e845cb4e2c0be8bc1bb760127accaf3a93
gptshubham <shubhamca76@gmail.com>
2023-12-16 14:11:08 +0530

    lec 2.1 complete , onley doubt session remains incomplete

--- W2.1-Promises and async JS revision, doubt session/main.js
+++ W2.1-Promises and async JS revision, doubt session/main.js
@@ -34,6 +34,8 @@ function onDone(){
 setTimeout(onDone,1000);
 console.log("after setTimeout....")
 */
+/*
+// fs.readFile("fileName","utf-8",function(err,data))
 let a = 1;
 console.log(a);
 const fs = require("fs");
@@ -46,3 +48,40 @@ for(let i=0;i<=1000000000;i++){
     ans += i;
 }
 console.log(ans);
+*/
+
+// writing our own async functions
+/* 
+// Approach 1 : Wrapping another async function (*callback hell)
+function myOwnSetTimeout(fn,duration){
+    setTimeout(fn,duration);
+}
+
+function logSomething(){
+    console.log("hi there..!");
+}
+myOwnSetTimeout(logSomething,2000);
+*/
+// Approach 2 : Promises
+// 1. defining asynchronous function --> 99% of the time you don't need to know how to defint them , what you really need to know is how to call them.
+function myOwnSetTimeout(duration){
+    let p = new Promise(function(resolve){
+        setTimeout(resolve,duration);
+    });
+    return p;
+}
+// function to be called back
+function logFirstThing(){
+    console.log("log the first thing...!");
+}
+
+// 2. Calling Asynchronous Function.
+
+// myOwnSetTimeout(2000)
+//     .then(logFirstThing);
+
+const ans = myOwnSetTimeout(3000);
+console.log(ans);
+ans.then(function(){
+    console.log("log the second thing...!");
+});
